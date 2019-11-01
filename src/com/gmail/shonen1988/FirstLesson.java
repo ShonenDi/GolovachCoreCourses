337bba91808d8831e48c74a0a4fed2ee0370dd04
Dima Usatyuk <shonen1988@gmail.com>
2019-11-01 16:31:19 +0200

    FirstLection 1master

--- src/com/gmail/shonen1988/FirstLesson.java
+++ src/com/gmail/shonen1988/FirstLesson.java
@@ -3,8 +3,10 @@ package com.gmail.shonen1988;
 public class FirstLesson {
     public static void main(String[] args) {
         String s = "Hello";
-        String s1 = new String(new char[]{'H','e','l','l','o'},0,5);
+//        String s1 = new String(new char[]{'H','e','l','l','o'},0,5);
         System.out.println(s);
-        System.out.println(s1);
+//        System.out.println(s1);
+        String s2 = s.substring(1, 4);
+        System.out.println(s2);
     }
 }
